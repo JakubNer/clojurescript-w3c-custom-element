f8be0e1016a6c8fca9b11459b0575452ea9b90ef
Jakub Ner <jakub_ner@hotmail.com>
2018-06-10 22:10:05 -0400

    fix build

--- src/kundel/element.cljs
+++ src/kundel/element.cljs
@@ -46,32 +46,32 @@
 
 ;; NO NEED TO MODIFY ANYTHING BELOW
 
-((def attrs (atom {})
+(def attrs (atom {}))
 
-  (defn ^:export created [this]
-    (let [_attrs (ctor-attrs)]
-      (doseq [keyval _attrs]
-        (swap! (val keyval) (get fns (key keyval)) (.getAttribute this (key keyval))))
-      (swap! attrs (merge @attrs {this _attrs}))
-      (r/render [c/render this _attrs] this)))      ;; attach reagent component
+(defn ^:export created [this]
+  (let [_attrs (ctor-attrs)]
+    (doseq [keyval _attrs]
+      (swap! (val keyval) (get fns (key keyval)) (.getAttribute this (key keyval))))
+    (swap! attrs (merge @attrs {this _attrs}))
+    (r/render [c/render this _attrs] this)))      ;; attach reagent component
 
-  (defn attached [this]) ;; not wired into reagent component
+(defn attached [this]) ;; not wired into reagent component
 
-  (defn ^:export detached [this] ;; not wired into reagent component, remove attributes tracked by this namespace for 'this'
-    (swap! attrs (dissoc @attrs this)))
+(defn ^:export detached [this] ;; not wired into reagent component, remove attributes tracked by this namespace for 'this'
+  (swap! attrs (dissoc @attrs this)))
 
-  (defn ^:export changed [this property-name old-value new-value]
-    (when-let [_attrs (get @attrs this)]
-      (swap! (get _attrs property-name) (get fns property-name) new-value)
-      (swap! attrs (merge @attrs {this _attrs}))))
+(defn ^:export changed [this property-name old-value new-value]
+  (when-let [_attrs (get @attrs this)]
+    (swap! (get _attrs property-name) (get fns property-name) new-value)
+    (swap! attrs (merge @attrs {this _attrs}))))
 
-  ;; register the w3c custom element.
-  (defn ^:export register []
-    (when (.-registerElement js/document)
-      (let [proto (.create js/Object (.-prototype js/HTMLElement))
-            proto' (go/create "createdCallback" #(this-as this (created this))
-                              "attachedCallback" #(this-as this (attached this))
-                              "detachedCallback" #(this-as this (detached this))
-                              "attributeChangedCallback" #(this-as this (changed this %1 %2 %3)))]
-        (go/extend proto proto')
-        (.registerElement js/document element-name #js{"prototype" proto}))))))
+;; register the w3c custom element.
+(defn ^:export register []
+  (when (.-registerElement js/document)
+    (let [proto (.create js/Object (.-prototype js/HTMLElement))
+          proto' (go/create "createdCallback" #(this-as this (created this))
+                            "attachedCallback" #(this-as this (attached this))
+                            "detachedCallback" #(this-as this (detached this))
+                            "attributeChangedCallback" #(this-as this (changed this %1 %2 %3)))]
+      (go/extend proto proto')
+      (.registerElement js/document element-name #js{"prototype" proto}))))
