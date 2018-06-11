9e04106ead758577084cf88bff394c2b90f72db1
Jakub Ner <jakub_ner@hotmail.com>
2018-06-10 22:06:55 -0400

    Modified web component to allow tracking of attributes per 'this' attached to the component: so we can have multiple components of ths type if we so choose.
    Call 'detach' to erase the per-component instance state.  (should be called automaticlaly if truly used as a web component in the dom and not called from JS the way we have to (because of Edge)).

--- src/kundel/element.cljs
+++ src/kundel/element.cljs
@@ -27,7 +27,8 @@
 ;;   Note that each 'attrs' has to have a corresponding 'fns' entry below.
 ;;
 ;; Modify these to suite your element:
-(def attrs {"some-text" (r/atom nil)})
+(defn ctor-attrs []
+  {"some-text" (r/atom nil)})
 
 ;; Custom translation functions for each attribute.
 ;; %1 is original property value, %2 is the new value.
@@ -45,25 +46,32 @@
 
 ;; NO NEED TO MODIFY ANYTHING BELOW
 
-(defn ^:export created [this]
-  (doseq [keyval attrs]
-    (swap! (val keyval) (get fns (key keyval)) (.getAttribute this (key keyval))))
-  (r/render [c/render this attrs] this))      ;; attach reagent component
+((def attrs (atom {})
 
-(defn attached [this]) ;; not wired into reagent component
+  (defn ^:export created [this]
+    (let [_attrs (ctor-attrs)]
+      (doseq [keyval _attrs]
+        (swap! (val keyval) (get fns (key keyval)) (.getAttribute this (key keyval))))
+      (swap! attrs (merge @attrs {this _attrs}))
+      (r/render [c/render this _attrs] this)))      ;; attach reagent component
 
-(defn detached [this]) ;; not wired into reagent component
+  (defn attached [this]) ;; not wired into reagent component
 
-(defn ^:export changed [this property-name old-value new-value]
-  (swap! (get attrs property-name) (get fns property-name) (.getAttribute this property-name)))
+  (defn ^:export detached [this] ;; not wired into reagent component, remove attributes tracked by this namespace for 'this'
+    (swap! attrs (dissoc @attrs this)))
 
-;; register the w3c custom element.
-(defn ^:export register []
-  (when (.-registerElement js/document)
-    (let [proto (.create js/Object (.-prototype js/HTMLElement))
-          proto' (go/create "createdCallback" #(this-as this (created this))
-                            "attachedCallback" #(this-as this (attached this))
-                            "detachedCallback" #(this-as this (detached this))
-                            "attributeChangedCallback" #(this-as this (changed this %1 %2 %3)))]
-      (go/extend proto proto')
-      (.registerElement js/document element-name #js{"prototype" proto}))))
+  (defn ^:export changed [this property-name old-value new-value]
+    (when-let [_attrs (get @attrs this)]
+      (swap! (get _attrs property-name) (get fns property-name) new-value)
+      (swap! attrs (merge @attrs {this _attrs}))))
+
+  ;; register the w3c custom element.
+  (defn ^:export register []
+    (when (.-registerElement js/document)
+      (let [proto (.create js/Object (.-prototype js/HTMLElement))
+            proto' (go/create "createdCallback" #(this-as this (created this))
+                              "attachedCallback" #(this-as this (attached this))
+                              "detachedCallback" #(this-as this (detached this))
+                              "attributeChangedCallback" #(this-as this (changed this %1 %2 %3)))]
+        (go/extend proto proto')
+        (.registerElement js/document element-name #js{"prototype" proto}))))))
